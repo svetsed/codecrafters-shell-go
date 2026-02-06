7fcc3255e40a2d759317eda3a726c3f0a3d2595c
svetsed <hellcatinthebox@gmail.com>
2026-02-06 22:24:20 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+
+	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v", err)
+		os.Exit(1)
+	}
+
+	cmd = strings.TrimSuffix(cmd, "\r")
+	cmd = strings.TrimSuffix(cmd, "\n")
+
+	fmt.Printf("%s: command not found\n", cmd)
 }
